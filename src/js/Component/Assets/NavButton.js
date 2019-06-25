e81a29e2d9ca6b3c37321bd019f2d4440250df14
Razvan Tanase <tangent.jotey@gmail.com>
2019-06-25 17:40:33 +0200

    update

--- src/js/Component/Assets/NavButton.js
+++ src/js/Component/Assets/NavButton.js
@@ -1,21 +1,20 @@
 Component.NavButton=function(){
     this.extends("Button");
     
+    console.log(this);
     this.classList.add("p-5");
     this.classList.remove("rounded");
     this.classList.add("border-r");
     this.addEventListener("click",e=>{
-        if(!this.dataset.view) return;
+        /*if(!this.dataset.view) return;
         content.module("Views/"+this.dataset.view);
-        state(this.dataset.state);
+        state(this.dataset.state);*/
     });
-
-    this.data={
-        sample: "hello",
-        list:[
-            {order: 0,state:"/home",view:"Home",text:"Home"},
-            {order: 2,state:"/about",view:"About",text:"About"},
-            {order: 1,state:"/contacts",view:"Contacts",text:"Contacts"}
-        ]
-    };
+    this.data={};
+    this.data.sample = "hello";
+    this.data.list= [
+        {order: 0,state:"/home",view:"Home",text:"Home"},
+        {order: 2,state:"/about",view:"About",text:"About"},
+        {order: 1,state:"/contacts",view:"Contacts",text:"Contacts"}
+    ];
 };
