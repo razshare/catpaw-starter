04829cdc6808471c93dc8ca4e3570b4df651b035
tncrazvan <tangent.jotey@gmail.com>
2019-07-22 00:57:43 +0200

    Update

--- public/templates/docs/examples/Hello/2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http;
-
-use com\github\tncrazvan\CatPaw\Http\HttpController;
-
-class Hello extends HttpController{
-    
-}
--- public/templates/docs/examples/Hello/3.php
+++ public/templates/docs/examples/Hello/3.php
@@ -2,8 +2,8 @@
 
 namespace App\Http;
 
-use com\github\tncrazvan\CatPaw\Http\HttpEvent;
 use com\github\tncrazvan\CatPaw\Http\HttpController;
+use com\github\tncrazvan\CatPaw\Http\HttpEvent;
 
 class Hello extends HttpController{
     public function &main(HttpEvent &$e, array &$path, string &$content){
