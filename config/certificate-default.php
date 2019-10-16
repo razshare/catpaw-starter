eb4123e819bd39da9f374b64dd214d4a19e9f441
Razvan Tanase <tangent.jotey@gmail.com>
2019-10-17 01:07:38 +0200

    update

---
+++ config/certificate-default.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "countryName" => "IT",
+    "stateOrProvinceName" => "PG",
+    "localityName" => "Perugia",
+    "organizationName" => "DevDungeon.com",
+    "organizationalUnitName" => "Development",
+    "commonName" => "DevDungeon",
+    "emailAddress" => "nanodano@devdungeon.com"
+];
