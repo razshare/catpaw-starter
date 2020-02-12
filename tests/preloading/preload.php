676fbb59a2442bc5432b61282f34eb17282dc155
tncrazvan <tangent.jotey@gmail.com>
2020-02-12 14:29:01 +0100

    update

--- tests/preloading/preload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function preload($c){
-    if(!opcache_compile_file("/mnt/c/Users/Razvan/Documents/PhpProjects/catpawskeleton/tests/preloading/classes/$c.php")){
-        trigger_error("Preloading failed",E_USER_ERROR);
-    }
-}
-
-preload("A");
