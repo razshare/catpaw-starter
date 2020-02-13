73261ccb0b72b87c6d999f2ec69537c0eb97cbad
Razvan <tangent.jotey@gmail.com>
2020-02-13 01:57:24 +0100

    update

--- src/http/user/Login.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\http\user;
-use com\github\tncrazvan\catpaw\http\HttpResponse;
-use com\github\tncrazvan\catpaw\http\HttpController;
-class Login extends HttpController{
-  public function main(){
-      return new HttpResponse([],"this is Login");
-  }
-}
