cb1ed868599f40a8dd241d2439f1f334eb571205
Razvan <tangent.jotey@gmail.com>
2020-02-15 11:18:27 +0100

    update£

--- src/http/test/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\http\test;
-use com\github\tncrazvan\catpaw\http\HttpResponse;
-use com\github\tncrazvan\catpaw\http\HttpController;
-class Test extends HttpController{
-  public function main(){
-      return new HttpResponse([],"this is Test");
-  }
-}
