31b966ebad7d3decf4f1bbc681633ec492d07cda
Razvan <tangent.jotey@gmail.com>
2020-08-25 00:12:02 +0200

    update

--- src/main.php
+++ src/main.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\events\http\homepage\HelloPage;
+use com\github\tncrazvan\catpaw\Event;
 use com\github\tncrazvan\catpaw\http\HttpConsumer;
 use com\github\tncrazvan\catpaw\http\HttpEvent;
 use com\github\tncrazvan\catpaw\tools\ServerFile;
@@ -11,65 +12,48 @@ use com\github\tncrazvan\catpaw\websocket\WebSocketEventOnClose;
 use com\github\tncrazvan\catpaw\websocket\WebSocketEventOnMessage;
 use com\github\tncrazvan\catpaw\websocket\WebSocketEventOnOpen;
 
-return [
-    "port" => 80,
-    "webRoot" => "../public",
-    "asciiTable" => true,
-    "bindAddress" => "127.0.0.1",
-    "httpMaxBodyLength" => 1024*1024*200,   //200 MB max read paylod for http requests.
-                                            //The above rule will not apply to http consumers,
-                                            //http consumers have no read payload limit because they free memory with each
-                                            //sequential read, which avoid memory overflow.
-    "httpMtu" => 1024*1024*5, //5 MB
-    "events" => [
-        "http"=>[
-            "@forward" => [ //forward requests to routes (order matters)
-                "/upload/{filename}" => "/asd/{filename}",
-                "/upload-old/{filename}" => "/asd-old/{filename}",
-            ],
-            "/asd-old/{filename}" => [
-                "POST" => function(string $filename, string &$body){
-                    if(!\file_exists("uploads/default"))
-                        mkdir("uploads/default",0777,true);
-                    $file = \fopen("./uploads/default/$filename",'a');
-                    \fwrite($file,$body);
-                    \fclose($file);
-    
-                    return "done";
-                }
-            ],
-            "/asd/{filename}" => [
-                "POST" => function(string $filename, string &$body, HttpConsumer $consumer){
-                    if(!\file_exists("uploads/consumer"))
-                        mkdir("uploads/consumer",0777,true);
-                    $file = \fopen("./uploads/consumer/$filename",'a');
-                    for($consumer->rewind();$consumer->valid();$consumer->consume($body)){
-                        \fwrite($file,$body);
-                        yield $consumer;
-                    }
-                    \fclose($file);
-    
-                    return "done";
-                }
-            ],
-            "/hello/{test}"  => fn(string $test,HttpEvent $e,HttpEventOnClose &$onCLose) => new HelloPage($test,$e,$onCLose),
-            "/templating/{username}" => function(string $username){
-                return ServerFile::include('../public/index.php',$username);
-            }  
-        ],
-        "websocket"=>[
-            "/test"
-                => fn(WebSocketEvent &$e,WebSocketEventOnOpen &$onOpen,WebSocketEventOnMessage &$onMessage,WebSocketEventOnClose &$onClose) 
-                    => new WebSocketTest($e,$onOpen,$onMessage,$onClose)
-        ]
-    ],
-    "sessionName" => "_SESSION",
-    "ramSession" => [
-        "allow" => false,
-        "size" => "1024M"
-    ],
-    "compress" => ["deflate"],
-    "headers" => [
-        "Cache-Control" => "no-store"
-    ]
-];
+Event::http('@forward',[
+    '/upload/{filename}' => '/asd/{filename}',
+    '/upload-old/{filename}' => '/asd-old/{filename}',
+]);
+
+Event::http('/asd-old/{filename}',[
+    'POST' => function(string $filename, string &$body){
+        if(!\file_exists("./uploads/default")){
+            mkdir("./uploads/default",0777,true);
+        }
+        $file = \fopen("./uploads/default/$filename",'a');
+        \fwrite($file,$body);
+        \fclose($file);
+
+        return "done";
+    }
+]);
+
+Event::http('/asd/{filename}',[
+    'POST' => function(string $filename, string &$body, HttpConsumer $consumer){
+        if(!\file_exists("./uploads/consumer")){
+            mkdir("./uploads/consumer",0777,true);
+        }
+        $file = \fopen("./uploads/consumer/$filename",'a');
+        for($consumer->rewind();$consumer->valid();$consumer->consume($body)){
+            \fwrite($file,$body);
+            yield $consumer;
+        }
+        \fclose($file);
+
+        return "done";
+    }
+]);
+
+Event::http('/hello/{test}',
+    fn(string $test,HttpEvent $e,HttpEventOnClose &$onClose) 
+        => new HelloPage($test,$e,$onClose));
+
+Event::http('/templating/{username}',function(string $username){
+    return ServerFile::include('../public/index.php',$username);
+});
+
+Event::websocket('/test',
+    fn(WebSocketEvent &$e,WebSocketEventOnOpen &$onOpen,WebSocketEventOnMessage &$onMessage,WebSocketEventOnClose &$onClose) 
+        => new WebSocketTest($e,$onOpen,$onMessage,$onClose));
