4966b6496914aca5abc25e4c3c2a7eaaa4828881
Razvan <tangent.jotey@gmail.com>
2020-10-09 23:17:25 +0200

    update

--- scripts/start.php
+++ scripts/start.php
@@ -12,64 +12,85 @@ use com\github\tncrazvan\catpaw\CatPaw;
 use com\github\tncrazvan\catpaw\tools\Dir;
 
 $_files_last_changed = [];
-$_last_error_shown = false;
-$_current_error = '';
-$_last_error_table = null;
 $count = isset($argv[3])?\intval($argv[3]):0;
-while(true){
-    try{
-        chdir(dirname(__FILE__).'/../src');
-        $config = require('main.php');
-        $server = new CatPaw($config,$count);
 
-        if(isset($argv[1]) && $argv[1] === 'dev'){
-            $delay = isset($argv[2])?\intval($argv[2]):100;
-            $server->listen(function() use (&$_files_last_changed,&$_last_error_shown,&$_current_error,&$delay){
-                $files = [];
-                Dir::findFilesRecursive(dirname(__FILE__)."/../src",$files);
-        
-                foreach($files as &$file){
-                    $name = $file['name'];
-                    if(isset($_files_last_changed[$name])){
-                        if($_files_last_changed[$name] < $file['lastChange']){
-                            $_last_error_shown = '';
-                            $_current_error = '';
-                            echo "Restarting server...\n";
-                            $_files_last_changed[$name] = $file['lastChange'];
-                            exit; //stop the server
-                        }
-                    }
-                    $_files_last_changed[$name] = $file['lastChange'];
-                }
-        
-                return $delay; //wait for 2000 ms
-            });
-        }else{
-            $server->listen();
-        }
-    }catch(\Throwable $e){
-        $trace = $e->getTraceAsString();
-        $code = $e->getCode();
-        $message = $e->getMessage();
-        $line = $e->getLine();
-        $file = $e->getFile();
-        $_current_error = "$trace\n$code\n$file\n$file\n$line\n$message";
-        if($_last_error_shown === '' || $_last_error_shown !== $_current_error){
-            $_last_error_table = new AsciiTable();
-            $_last_error_table->style(0,[
-                "width"=>50
-            ]);
-            $_last_error_table->style(1,[
-                "width"=>200
-            ]);
+function manage_throwable(\Throwable $e){
+    $trace = $e->getTraceAsString();
+    $code = $e->getCode();
+    $message = $e->getMessage();
+    $line = $e->getLine();
+    $file = $e->getFile();
 
-            echo "$trace\n";
-            $_last_error_table->add("Code",$code);
-            $_last_error_table->add("File",$file);
-            $_last_error_table->add("Line",$line);
-            $_last_error_table->add("Message",$message);
-            echo $_last_error_table->toString()."\n";
-            $_last_error_shown = $_current_error;
+    $table = new AsciiTable();
+    $table->style(0,[
+        "width"=>50
+    ]);
+    $table->style(1,[
+        "width"=>128
+    ]);
+
+    $message_pieces = \preg_split('/\n/',$message);
+    $message = '';
+    $size = 0;
+    foreach($message_pieces as &$piece){
+        $l_size = strlen($piece);
+        $size += $l_size;
+        if($size >= 127){
+            $message .="\n";
+            $size = $l_size;
         }
+        $message .=$piece;
+    }
+
+    echo "$trace\n";
+    $table->add("Code",$code);
+    $table->add("File",$file);
+    $table->add("Line",$line);
+    $table->add("Message",$message);
+    echo $table->toString()."\n";
+}
+
+function check_file_change(&$_files_last_changed,bool $exit_immediately = true){
+    $files = [];
+    Dir::findFilesRecursive(dirname(__FILE__)."/../src",$files);
+
+    foreach($files as &$file){
+        $name = $file['name'];
+        if(isset($_files_last_changed[$name])){
+            if($_files_last_changed[$name] < $file['lastChange']){
+                echo "Restarting server...\n";
+                $_files_last_changed[$name] = $file['lastChange'];
+                exit; //stop the server
+            }
+        }
+        $_files_last_changed[$name] = $file['lastChange'];
+    }
+}
+
+set_error_handler(
+    function ($severity, $message, $file, $line) {
+        throw new ErrorException($message, $severity, $severity, $file, $line);
+    }
+);
+
+try{
+    chdir(dirname(__FILE__).'/../src');
+    $config = @require('main.php');
+    $server = new CatPaw($config,$count);
+
+    if(isset($argv[1]) && $argv[1] === 'dev'){
+        $delay = isset($argv[2])?\intval($argv[2]):100;
+        $server->listen(function() use (&$_files_last_changed,&$delay){
+            check_file_change($_files_last_changed,false);
+            return $delay; //wait for $delay ms
+        });
+    }else{
+        $server->listen();
+    }
+}catch(\Throwable $e){
+    manage_throwable($e);
+    while(true){
+        check_file_change($_files_last_changed);
+        usleep(10000);
     }
 }
--- src/main.php
+++ src/main.php
@@ -1,23 +1,19 @@
 <?php
 return [
-    "port" => 80, 
+    "port" => 80,
     "webRoot" => "../public",
     "sessionName" => "../_SESSION",
+    "asciiTable" => false,
     "events" => [
         "http"=>[
             "@forward" => [
                 '/upload/{filename}' => '/asd/{filename}',
                 '/upload-old/{filename}' => '/asd-old/{filename}',
             ],
-
             "/home"                     => [    "GET"   =>      require './api/http/get_home.php'      ],
-
             "/asd-old/{filename}"       => [    "POST"  =>      require './api/http/post_asd_old.php'  ],
-
             "/asd/{filename}"           => [    "POST"  =>      require './api/http/post_asd.php'      ],
-            
             "/hello/{test}"             => [    "GET"   =>      require './api/http/get_hello.php'     ],
-
             "/templating/{username}"    => [    "GET"   =>      require './api/http/templating.php'    ],
         ],
         "websocket"=>[
