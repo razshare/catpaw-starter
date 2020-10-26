e6f69664cb3e982cae11da108b051a71826a55de
Razvan Tanase <tangent.jotey@gmail.com>
2020-10-26 18:57:00 +0100

    update

--- src/main.php
+++ src/main.php
@@ -3,8 +3,8 @@
 use com\github\tncrazvan\catpaw\tools\helpers\Route;
 
 Route::get("/home",                     require './api/http/get_home.php');
-Route::get("/file-old/{filename}",      require './api/http/post_file_old.php');
-Route::get("/file/{filename}",          require './api/http/post_file.php');
+Route::post("/file-old/{filename}",      require './api/http/post_file_old.php');
+Route::post("/file/{filename}",          require './api/http/post_file.php');
 Route::get("/hello/{test}",             require './api/http/get_hello.php');
 Route::get("/templating/{username}",    require './api/http/templating.php');
 
