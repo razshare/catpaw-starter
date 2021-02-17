44a3840bc06bb02cd2f7a99f85ca456510560309
Razvan <tangent.jotey@gmail.com>
2021-02-18 00:32:24 +0100

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,19 +1,2 @@
 <?php
-
-use com\github\tncrazvan\catpaw\attributes\Body;
-use com\github\tncrazvan\catpaw\attributes\Consumes;
-use com\github\tncrazvan\catpaw\attributes\sessions\Session;
-use com\github\tncrazvan\catpaw\tools\helpers\Route;
-
-Route::post(
-    "/",
-    #[Consumes("application/json")]
-    function(
-        #[Session]
-        array &$session, 
-        #[Body]
-        array $body
-    ){
-    //update the user's username
-    $session["username"] = $body["username"];
-});
+echo "helo world\n";
