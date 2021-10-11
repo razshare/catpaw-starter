5c139608196e48b3782aff324e73c3a0fa889cba
Razvan <tangent.jotey@gmail.com>
2021-10-11 10:30:43 +0200

    update

--- src/main.php
+++ src/main.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace app;
 
+use net\razshare\catpaw\attributes\sessions\Session;
 use net\razshare\catpaw\tools\helpers\Route;
 use function app\modules\test;
 
-Route::get("/api/v1/test",function(){
-    return test();
+Route::get("/api/v1/test", function (
+	#[Session] array &$session
+){
+	return test();
 });
