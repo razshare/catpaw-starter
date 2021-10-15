a5ae9cbca12affcfff516f97eb93c10b32fecf95
Razvan <tangent.jotey@gmail.com>
2021-10-15 21:17:31 +0200

    update

--- src/main.php
+++ src/main.php
@@ -1,6 +1,10 @@
 <?php
+
+use CatPaw\CatPaw;
+use CatPaw\Configs\MainConfiguration;
 use CatPaw\Tools\Helpers\Route;
 
-function main(){
-	yield Route::get("/hello-world", fn()=> "hello world");
+function main(MainConfiguration $config): Generator {
+	yield Route::get("/hello-world", fn()=> test());
+	yield CatPaw::start($config);
 }
---
+++ src/test.module.php
@@ -0,0 +1,5 @@
+<?php
+
+function test():string{
+	return "test";
+}
