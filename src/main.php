fa1106e075d633977b9c90886d3863cc52709d0c
Razvan <tangent.jotey@gmail.com>
2021-10-17 21:11:06 +0200

    release 2.0.4

--- src/main.php
+++ src/main.php
@@ -2,12 +2,13 @@
 
 namespace {
 
+	use CatPaw\Attributes\Inject;
 	use CatPaw\CatPaw;
 	use CatPaw\Configs\MainConfiguration;
 	use CatPaw\Tools\Helpers\Route;
 
-	function main(MainConfiguration $config): Generator {
+	function main(#[Inject] MainConfiguration $config): Generator {
 		yield Route::get("/hello-world", fn() => "hello world");
-		yield CatPaw::start($config);
+		yield CatPaw::startWebServer($config);
 	}
 }
