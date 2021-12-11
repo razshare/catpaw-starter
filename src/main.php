144030a1017934b93eac0523a68320c26808a590
Razvan <tangent.jotey@gmail.com>
2021-12-11 23:08:23 +0100

    hello world example simplified

--- src/main.php
+++ src/main.php
@@ -2,14 +2,14 @@
 
 namespace {
 
-	use App\TestService;
 	use CatPaw\CatPaw;
 	use CatPaw\Configs\MainConfiguration;
 	use CatPaw\Tools\Helpers\Route;
 
 	function main(MainConfiguration $config): Generator {
 		yield CatPaw::startWebServer($config);
-		yield Route::get("/hello-world", fn(TestService $test) => $test->test());
+		
+		yield Route::get("/hello", fn() => "hello world");
 
 		echo Route::describe();
 	}
