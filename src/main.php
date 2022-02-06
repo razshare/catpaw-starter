c4fcfb9d83afc1a18b9adf4e381394879e22b564
Razvan <tangent.jotey@gmail.com>
2022-02-06 07:03:07 +0100

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,13 +1,6 @@
 <?php
-
 namespace {
-
-    use CatPaw\Web\Attribute\Http\StartWebServer;
-    use CatPaw\Web\Utility\Route;
-
-	#[StartWebServer]
 	function main() {
-		Route::get("/plain", fn() => "this is plain text.");
-		echo Route::describe();
+		echo "hello world\n";
 	}
 }
