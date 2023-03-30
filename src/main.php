08625a18d74cbc9d5bc24deb4b493804c7c7a1d8
Razvan Tanase <tangent.jotey@gmail.com>
2023-03-30 15:38:09 +0200

    Version 0.4.142

--- src/main.php
+++ src/main.php
@@ -1,11 +1,4 @@
 <?php
-
-use function CatPaw\readline;
-
 function main() {
-    $username = yield readline("What is your username? ");
-    echo "username: $username";
-
-    $password = yield readline("What is your password? ");
-    echo "password: $password";
+    echo "hello world\n";
 }
